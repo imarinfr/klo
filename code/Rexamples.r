7e3cf744a8cee0d83ee568b46bf84c75cc0cfaad
imarinfr <imarinfr@optocom.es>
2021-05-12 18:35:56 +0200

    reworked the file completely to add the final example used in the JSS manuscript.

--- code/Rexamples.r
+++ code/Rexamples.r
@@ -1,90 +1,55 @@
-# Script to obtain sample estimates of differential entropy and mutual
-# information from trivariate data. Example 1 uses the same scene used
-# for Figure 1. Example 2 focuses on processing scene data to illustrate
-# effects of different illuminations on information.
+# This example shows how to compute the Gaussian approximation and the KL and KLo
+# estimates of differential entropy and of mutual information between two sets of
+# trivariate data. Each set represents a color image of a scene recorded at
+# successive instants, about 1 minute apart, shown in Figure 1 of the manuscript
+# (other images are available with the software). The image values were expressed
+# not as conventional RGB triplets but as LMS triplets, corresponding to activities
+# in the long-, medium-, and short-wavelength-sensitive cone photoreceptors of the eye.
+# Each data set is stored as a 1005x1306x3 array, where the first two dimensions index
+# pixel coordinates and the the third dimension indexes LMS values.
 
-# set the working directory to the subfolder "code" in the GIT checkout
-# directory.
+# To run this example, we need to set the working directory to the folder where the
+# https://github.com/imarinfr/klo repository has been checked out.
 #setwd(<CHECKOUT DIRECTORY>)
 
-# load the necessary libraries
-library(klo)      # the KLo package
-library(R.matlab) # to load MatLab files in R
-
-# EXAMPLE 1
-# Load 1005 x 1306 x 3 scene data coded as LMS (long-, medium-, and
-# short-wavelength) cone photoreceptor responses of human eye at each pixel
-lms1 <- readMat("data/lms_sete_fontes_1333.mat")$lms1
-lms2 <- readMat("data/lms_sete_fontes_1335.mat")$lms2
-
-# Reshape LMS scene data into 1312530 x 3 arrays
+# The first step is to load and reformat the each data set into three columns of
+# LMS values lms1 and lms2
+library(klo)
+library(R.matlab)
+lms1 <- readMat("klo/data/lms_sete_fontes_1333.mat")$lms1
+lms2 <- readMat("klo/data/lms_sete_fontes_1335.mat")$lms2
 nr <- dim(lms1)[1]
 nc <- dim(lms1)[2]
 nw <- dim(lms1)[3]
 dim(lms1) <- c(nr * nc, nw)
 dim(lms2) <- c(nr * nc, nw)
 
-# Get Gaussian and KL estimates of differential entropy without and
-# with offset correction
-print(entg(cbind(lms1, lms2)))
-print(entkl(cbind(lms1, lms2), "kl"))
-print(entkl(cbind(lms1, lms2), "klo"))
+# Here are the first few rows of lms1 and lms2. Values are less than unity because
+# of the way LMS values are normalized.
+head(lms1)
+head(lms2)
 
-# Get Gaussian and KL estimates of mutual information without and
-# with the offset correction
-print(mig(lms1, lms2))
-print(mikl(lms1, lms2, "kl"))
-print(mikl(lms1, lms2, "klo"))
+# The initial Gaussian estimate of differential entropy for the first image is
+# obtained thus:
+entg(lms1)
 
-###########################################################################
-# EXAMPLE 2
-# Now try similar calculation but using hyperspectral reflectance images of
-# scene under different illuminants and viewed by camera with different
-# sensor spectra or by human eye. For more details see following:
-# https://personalpages.manchester.ac.uk/staff/david.foster/Tutorial_Colour_Information/Tutorial_Color_Information.html
+# These values are negative because the data values are less than unity. The KL
+# estimates of differential entropy without and then with offset are obtained
+# similarly:
+entkl(lms1, "kl")
+entkl(lms1, "klo")
 
-# Load 255 x 335 x 33 scene spectral reflectance data (33 wavelengths)
-reflectances <- readMat("data/ref4_scene5.mat")$reflectances
-nr <- dim(reflectances)[1]
-nc <- dim(reflectances)[2]
-nw <- dim(reflectances)[3]
+# The initial Gaussian estimate of mutual information between the two images is
+# obtained thus:
+mig(lms1, lms2)
 
-# Load daylight illuminants of correlated color temperature 25000 K and
-# 4000 K; other daylight illuminant combinations can be tested for their
-# effect on mutual information, e.g. 6500 K
-illum_25000 <- readMat("data/illum_25000.mat")$illum.25000
-illum_4000 <-readMat("data/illum_4000.mat")$illum.4000
+# The estimate is non-negative, as expected. The KL estimates of mutual information
+# without and then with offset are obtained similarly:
+mikl(lms1, lms2, "kl")
+mikl(lms1, lms2, "klo")
 
-# Convert to 255 x 335 x 33 scene spectral radiances for each illuminant
-# (wavelength for-loop is used for clarity, not efficiency)
-radiances_25000 <- array(rep(NA, nr * nc * nw), dim = c(nr, nc, nw))
-radiances_4000  <- array(rep(NA, nr * nc * nw), dim = c(nr, nc, nw))
-for(i in 1:33) {
-  radiances_25000[,,i] <- reflectances[,,i] * illum_25000[i]
-  radiances_4000[,,i]  <- reflectances[,,i] * illum_4000[i]
-}
-
-# Reshape scene data into 85425 x 3 arrays
-dim(radiances_25000) <- c(nr * nc, nw)
-dim(radiances_4000)  <- c(nr * nc, nw)
-
-# Get 33 x 4 RGB sensor curves for chosen sensor, here 'agilent'
-# Remove first column wavelength vector as implicit in data structure
-rgbsens <- rgbcurves("agilent")[,-1]
-rgb_25000 <- radiances_25000 %*% rgbsens
-rgb_4000  <- radiances_4000 %*% rgbsens
-
-# Gaussian and KL estimates of differential entropy without and
-# with the offset correction
-print(entg(cbind(rgb_25000, rgb_4000)))
-print(entkl(cbind(rgb_25000, rgb_4000), 'kl'))
-print(entkl(cbind(rgb_25000, rgb_4000), 'klo'))
-
-# Gaussian and KL estimates of mutual information without and
-# with the offset correction
-print(mig(rgb_25000, rgb_4000))
-print(mikl(rgb_25000, rgb_4000, 'kl'))
-print(mikl(rgb_25000, rgb_4000, 'klo'))
+# Notice the progressive increase in the size of the mutual information estimates
+# with the Gaussian approximation, the KL estimator, and then the KLo estimator
 
 ###########################################################################
-# (c) Ivan Marin-Franch and David H Foster 10-Jul-20
+# (c) Ivan Marin-Franch and David H Foster 12-May-21
