4b7b5c78ea01ccaa50196db2c06f3fdbe747d886
imarinfr <imarinfr@optocom.es>
2021-11-23 23:12:41 +0100

    changed the example scripts in MatLab, R, and Python, so that they all do the same as in the Signal Processing draft.

---
+++ code/matlab/example.m
@@ -0,0 +1,53 @@
+% This example shows how to compute the Gaussian approximation and the KL
+% and KLo estimates of differential entropy and of mutual information
+% between two sets of trivariate data. Each set represents a color image of
+% a scene recorded at successive instants, about 1 minute apart, shown in
+% Figure 1 of the manuscript (other images are available with the
+% software). The image values were expressed not as conventional RGB
+% triplets but as LMS triplets, corresponding to activities in the long-,
+% medium-, and short-wavelength-sensitive cone photoreceptors of the eye.
+% Each data set is stored as a 507x657x3 array, where the first two
+% dimensions index pixel coordinates and the third dimension indexes LMS
+% values.
+
+% To run this example, we need to set the working directory to the
+% subfolder code/matlab/ in the folder where the
+% https://github.com/imarinfr/klo repository has been checked out.
+
+% The first step is to load and reformat each data set into three columns
+% of LMS values lms1 and lms2
+load ../../data/lms_sete_fontes_1320.mat;
+load ../../data/lms_sete_fontes_1321.mat;
+[nr, nc, nw] = size(lms1);
+lms1 = reshape(lms1, nr * nc, nw);
+lms2 = reshape(lms2, nr * nc, nw);
+
+% Here are the first few rows of lms1 and lms2. Values are less than unity
+% because of the way LMS values are normalized.
+lms1(1:6,:)
+lms2(1:6,:)
+
+% The initial Gaussian estimate of differential entropy for the first image
+% is obtained thus:
+entg(lms1)
+
+% These values are negative because the data values are less than unity.
+% The KL estimates of differential entropy without and then with offset are
+% obtained similarly:
+entkl(lms1, 'kl')
+entkl(lms1, 'klo')
+
+% The initial Gaussian estimate of mutual information between the two
+% images is obtained thus:
+mig(lms1, lms2)
+
+% The estimate is non-negative, as expected. The KL estimates of mutual
+% information without and then with offset are obtained similarly:
+mikl(lms1, lms2, 'kl')
+mikl(lms1, lms2, 'klo')
+
+% Notice the progressive increase in the size of the mutual information
+% estimates with the KL estimator and the KLo estimator
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) Ivan Marin-Franch and David H Foster 12-May-21
--- code/python/example.py
+++ code/python/example.py
@@ -10,45 +10,46 @@
 # dimensions index pixel coordinates and the third dimension indexes LMS
 # values.
 
-# To run this example, we need to set the working directory to the folder
-# where the https://github.com/imarinfr/klo repository has been checked out.
-#setwd(<CHECKOUT DIRECTORY>)
+# Import functions necessary to run entkl and mikl
+from numpy import empty, reshape, concatenate
+from scipy.io import loadmat
+from mikl import entg, mig, entkl, mikl
+
+# To run this example, we need to set the working directory to the
+# subfolder code/python/ in the folder where the
+# https://github.com/imarinfr/klo repository has been checked out.
 
 # The first step is to load and reformat each data set into three columns
 # of LMS values lms1 and lms2
-library(klo)
-library(R.matlab)
-lms1 <- readMat("klo/data/lms_sete_fontes_1320.mat")$lms1
-lms2 <- readMat("klo/data/lms_sete_fontes_1321.mat")$lms2
-nr <- dim(lms1)[1]
-nc <- dim(lms1)[2]
-nw <- dim(lms1)[3]
-dim(lms1) <- c(nr * nc, nw)
-dim(lms2) <- c(nr * nc, nw)
+lms1 = loadmat("../../data/lms_sete_fontes_1320.mat")["lms1"]
+lms2 = loadmat("../../data/lms_sete_fontes_1321.mat")["lms2"]
+nr, nc, nw = lms1.shape
+lms1 = reshape(lms1, (nr * nc, nw), order="F")
+lms2 = reshape(lms2,  (nr * nc, nw), order="F")
 
 # Here are the first few rows of lms1 and lms2. Values are less than unity
 # because of the way LMS values are normalized.
-head(lms1)
-head(lms2)
+print(lms1[:6,:])
+print(lms2[:6,:])
 
 # The initial Gaussian estimate of differential entropy for the first image
 # is obtained thus:
-entg(lms1)
+print(entg(lms1))
 
 # These values are negative because the data values are less than unity.
 # The KL estimates of differential entropy without and then with offset are
 # obtained similarly:
-entkl(lms1, "kl")
-entkl(lms1, "klo")
+print(entkl(lms1, "kl"))
+print(entkl(lms1, "klo"))
 
 # The initial Gaussian estimate of mutual information between the two
 # images is obtained thus:
-mig(lms1, lms2)
+print(mig(lms1, lms2))
 
 # The estimate is non-negative, as expected. The KL estimates of mutual
 # information without and then with offset are obtained similarly:
-mikl(lms1, lms2, "kl")
-mikl(lms1, lms2, "klo")
+print(mikl(lms1, lms2, "kl"))
+print(mikl(lms1, lms2, "klo"))
 
 # Notice the progressive increase in the size of the mutual information
 # estimates with the KL estimator and the KLo estimator
